6fa29e00ab946994ba76c830490689bb523380de
Sebas <desarrolladorfs2@neurum.com.co>
2026-05-29 10:35:03 -0700

    env full bd rds

--- src/config/db.js
+++ src/config/db.js
@@ -7,6 +7,7 @@ export const pool = mysql.createPool({
   user: env.DB_USER,
   password: env.DB_PASSWORD,
   database: env.DB_NAME,
+  ssl: { rejectUnauthorized: false },
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
