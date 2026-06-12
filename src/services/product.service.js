6f2199b678bcd0cd1ef6d50d9ee0057daaba7f87
Sebas <desarrolladorfs2@neurum.com.co>
2026-06-12 14:23:09 -0500

    income funtion update

--- src/services/product.service.js
+++ src/services/product.service.js
@@ -167,13 +167,13 @@ export async function listProducts(search = '', idLaboratorio = null, lote = '')
      ) stock ON stock.id_producto = p.id_producto
      WHERE p.sku IS NOT NULL AND p.sku != ''
        AND p.nombre_comercial IS NOT NULL AND p.nombre_comercial != ''
-       AND (? = '' OR p.nombre_comercial LIKE ? OR p.sku LIKE ? OR p.principio_activo LIKE ? OR p.codigo_barras LIKE ?)
+       AND (? = '' OR p.nombre_comercial LIKE ? OR p.sku LIKE ? OR p.principio_activo LIKE ? OR p.codigo_barras LIKE ? OR p.codigo_control LIKE ?)
        AND (? IS NULL OR p.id_laboratorio = ?)
        AND (? = '' OR EXISTS (
              SELECT 1 FROM lotes l WHERE l.id_producto = p.id_producto AND l.numero_lote LIKE ?
            ))
      ORDER BY p.nombre_comercial ASC`,
-    [env.PUBLIC_UPLOAD_BASE_URL, search, wildcard, wildcard, wildcard, wildcard, idLaboratorio, idLaboratorio, lote, loteWildcard]
+    [env.PUBLIC_UPLOAD_BASE_URL, search, wildcard, wildcard, wildcard, wildcard, wildcard, idLaboratorio, idLaboratorio, lote, loteWildcard]
   );
 }
 
